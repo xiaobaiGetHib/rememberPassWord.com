2110d44ec5de93a5eabf0635457556b11612981c
xiaobai <2633482531@qq.com>
2018-04-13 17:53:33 +0800

    初次提交1

--- app/src/main/java/com/xiaobai/rpass/rememberpass/act/ViewLogActivity.java
+++ app/src/main/java/com/xiaobai/rpass/rememberpass/act/ViewLogActivity.java
@@ -2,12 +2,10 @@ package com.xiaobai.rpass.rememberpass.act;
 
 
 import android.os.Bundle;
-import android.text.TextUtils;
 
 import com.xiaobai.rpass.rememberpass.R;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+
 
 public class ViewLogActivity extends BaseActivity {
 
@@ -19,26 +17,5 @@ public class ViewLogActivity extends BaseActivity {
 
     }
 
-    public static String md5(String string) {
-        if (TextUtils.isEmpty(string)) {
-            return "";
-        }
-        MessageDigest md5 = null;
-        try {
-            md5 = MessageDigest.getInstance("MD5");
-            byte[] bytes = md5.digest(string.getBytes());
-            String result = "";
-            for (byte b : bytes) {
-                String temp = Integer.toHexString(b & 0xff);
-                if (temp.length() == 1) {
-                    temp = "0" + temp;
-                }
-                result += temp;
-            }
-            return result;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
+
 }
